ff02c2dd708753d94a2b3d8db6bd5a7a552077b0
Chiamaka Onyia <98760959+Techxie-bc@users.noreply.github.com>
2023-02-12 18:13:51 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function multiplyDigits(a, n){
+  a * b
+};
+let product = multiplyDigits(5, 6);
+console.log(product);
+
+• In the same index.js file, create variables for your first name, last name, Stutern track, skills, favourite color, year of your country’s independence. Use these variables to form a sentence and log the sentence in the console.
+let firstName = "Chiamaka";
+let lastName = "Onyia";
+let track = "Frontend Development";
+let skills = "coding";
+let favColor = "blue";
+let year = "1960";
+
+console.log("My name is: " firstName lastName". I am studying: " track "I love: " coding " . My favourite color is: " favColor " My country got independence in the year: " year);
