071a09b818bf5076ccd5f6a054a1ff6a84b7373a
Chiamaka Onyia <98760959+Techxie-bc@users.noreply.github.com>
2023-02-12 18:15:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ function multiplyDigits(a, n){
 let product = multiplyDigits(5, 6);
 console.log(product);
 
-• In the same index.js file, create variables for your first name, last name, Stutern track, skills, favourite color, year of your country’s independence. Use these variables to form a sentence and log the sentence in the console.
 let firstName = "Chiamaka";
 let lastName = "Onyia";
 let track = "Frontend Development";
