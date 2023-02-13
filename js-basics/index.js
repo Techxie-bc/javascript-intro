fa0378c7fad527dd381aad17d37b0bff6c31381b
Techxie-bc <donnaogbu@gmail.com>
2023-02-13 13:41:03 +0100

    done

---
+++ js-basics/index.js
@@ -0,0 +1,14 @@
+function multiplyDigits(a, b){
+  let product = a + b;
+  console.log(product);
+};
+multiplyDigits(5, 6);
+
+let firstName = "Chiamaka";
+let lastName = "Onyia";
+let track = "Frontend Development";
+let skills = "coding";
+let favColor = "blue";
+let year = "1960";
+
+console.log("My name is: " + firstName + lastName + ". I am studying: " + track + " I love: " + skills + " . My favourite color is: " + favColor + " My country got independence in the year: " + year);
